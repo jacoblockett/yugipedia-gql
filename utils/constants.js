db91052d2f919afb0d35154cdd1a43f9009f522c
jacoblockett <jacobwyattlockett@gmail.com>
2024-02-12 01:44:02 -0500

    added wiki endpoint for scraping

--- utils/constants.js
+++ utils/constants.js
@@ -1,3 +1,4 @@
 export const ENDPOINT = "https://yugipedia.com/api.php"
 export const MEDIA_ENDPOINT = "https://yugipedia.com/wiki/Special:FilePath"
 export const SPECIAL_BROWSE_ENDPOINT = "https://yugipedia.com/wiki/Special:Browse"
+export const WIKI_ENDPOINT = "https://yugipedia.com/wiki"
