6ac7a2dbf76d62f628a41a8b4716149ac2a062f6
jacoblockett <jacobwyattlockett@gmail.com>
2024-02-13 00:07:46 -0500

    file name change for toProperCase.js -> properCase.js

--- format/formatCardData.js
+++ format/formatCardData.js
@@ -1,6 +1,6 @@
 import camelCase from "lodash.camelcase"
 import keysToCamelCase from "../utils/keysToCamelCase.js"
-import properCase from "../utils/toProperCase.js"
+import properCase from "../utils/properCase.js"
 import ruby from "../utils/ruby.js"
 import cleanLinkedText from "../utils/cleanLinkedText.js"
 import { MEDIA_ENDPOINT } from "../utils/constants.js"
